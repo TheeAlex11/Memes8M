2e34611a4379ebd0f398cdbb741d89222a09e928
PortatilDAW <thealex11pro@gmail.com>
2026-03-10 20:36:41 +0100

    Fix rutas repo

--- tests/TestPuntuacion.java
+++ tests/TestPuntuacion.java
@@ -1,7 +1,7 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-class PuntuacionTest {
+class TestPuntuacion {
 
     @Test
     void getNombre_debeRetornarElNombre() {
